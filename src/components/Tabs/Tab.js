712a1201a0a38f7ec2dcc9f2c1f07dc17017311a
Kyaw <40131560+kiowd@users.noreply.github.com>
2020-11-13 00:02:32 +0000

    click-events-have-key-events

---
+++ src/components/Tabs/Tab.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Tab = ({ clickTabItem, activeTab, label }) => {
+  //   const handleOnClick = () => {
+  //     onClick(label);
+  //   };
+
+  let className = "tab-list-item";
+
+  if (activeTab === label) {
+    className += " tab-list-active";
+  }
+
+  return (
+    <li
+      className={className}
+      onClick={() => clickTabItem(label)}
+      aria-hidden="true"
+    >
+      {label}
+    </li>
+  );
+};
+
+Tab.propTypes = {
+  activeTab: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  clickTabItem: PropTypes.func.isRequired,
+};
+
+export default Tab;
