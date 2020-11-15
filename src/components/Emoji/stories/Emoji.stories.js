6a74b4e06fd9b4b80277df9bcf9a24e1ceac8498
Tayfun Aydogan <40833117+tayfuntayfun@users.noreply.github.com>
2020-11-15 14:38:27 +0000

    Styling Emojis

--- src/components/Emoji/stories/Emoji.stories.js
+++ src/components/Emoji/stories/Emoji.stories.js
@@ -2,22 +2,38 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSmile, faMeh, faFrown } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
 import Emoji from "..";
 
 library.add(faSmile, faMeh, faFrown);
+
+// const = styled.div`
+//   .ferhat {
+//     color: "yellow";
+//     height: 200px;
+//   }
+// `;
+
+const StyledIcon = styled(FontAwesomeIcon)`
+  .icons {
+    color: "yellow";
+  }
+`;
 
 export default {
   title: "Components/Emoji",
   component: Emoji,
 };
 
-export const AddEmoji = () => {
-  const icons = ["smile", "meh", "frown"];
-  return (
-    <div>
-      {icons.map((item) => (
-        <FontAwesomeIcon icon={item} />
-      ))}
-    </div>
-  );
+//
+export const SmileEmoji = () => {
+  return <StyledIcon icon="smile" style={{ color: "red" }} />;
+};
+
+export const MehEmoji = () => {
+  return <StyledIcon icon="meh" className="icons" />;
+};
+
+export const FrownEmoji = () => {
+  return <StyledIcon icon="frown" />;
 };
