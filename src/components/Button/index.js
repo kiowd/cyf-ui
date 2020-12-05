76ce94c37f87c0f621de4e2161633291d9f96815
Tayfun Aydogan <40833117+tayfuntayfun@users.noreply.github.com>
2020-12-05 13:57:54 +0000

    Label-BgColor-Color Controls Added

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,55 +1,136 @@
 import React from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 export const Button = styled.button`
-  font-family: Lato;
   font-style: normal;
-  font-weight: 900;
-  font-size: 28px;
-  line-height: 21px;
-  color: white;
 `;
 
 export const StyledPrimaryButton = styled.button`
-  background: #3455db;
-  border: 3px solid #3455db;
-  box-sizing: border-box;
-  border-radius: 2px;
+  width: 135.88px;
+  left: 2354px;
+  top: 1432px;
+  color: ${(props) => (props.color ? props.color : `#ffffff`)};
+  background-color: ${(props) =>
+    props.background ? props.background : `#00a3ff`};
+  border-color: #00a3ff;
+  font-weight: 400;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  border: 1px solid transparent;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  border-radius: 0.25rem;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 `;
 
 export const StyledSecondaryButton = styled.button`
   width: 135.88px;
-  height: 70px;
-  left: 2144px;
-  top: 1410px;
-
-  background: #3455db;
+  left: 2354px;
+  top: 1432px;
+  color: ${(props) => (props.color ? props.color : `#ffffff`)};
+  background-color: ${(props) =>
+    props.background ? props.background : `#28a745`};
+  border-color: #28a745;
+  font-weight: 400;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  border: 1px solid transparent;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  border-radius: 0.25rem;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 `;
 
 export const StyledDangerButton = styled.button`
-  width: 284.36px;
-  height: 27.65px;
+  width: 135.88px;
   left: 2354px;
   top: 1432px;
-`;
+  color: ${(props) => (props.color ? props.color : `#ffffff`)};
+  background-color: ${(props) =>
+    props.background ? props.background : `#dc3545`};
+  font-weight: 400;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  border: 1px solid transparent;
+  padding: .375rem .75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  border-radius: .25rem;
+  transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+}`;
 
 export const StyledOutlinedButton = styled.button`
-  color: blue;
-  font-size: 24px;
+  width: 135.88px;
+  left: 2354px;
+  top: 1432px;
+  color: ${(props) => (props.color ? props.color : `#007bff`)};
+  background-color: ${(props) =>
+    props.background ? props.background : `#ffffff`};
+  font-weight: 400;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  border: 1px solid #007bff;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  border-radius: 0.25rem;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 `;
 
-export const PrimaryButton = () => (
-  <StyledPrimaryButton>Primary </StyledPrimaryButton>
+export const PrimaryButton = ({ label, background, color }) => (
+  <StyledPrimaryButton background={background} color={color}>
+    {label}
+  </StyledPrimaryButton>
 );
 
-export const SecondaryButton = () => (
-  <StyledSecondaryButton>Secondary </StyledSecondaryButton>
+export const SecondaryButton = ({ label, background, color }) => (
+  <StyledSecondaryButton background={background} color={color}>
+    {label}{" "}
+  </StyledSecondaryButton>
 );
 
-export const DangerButton = () => (
-  <StyledDangerButton>Danger </StyledDangerButton>
+export const DangerButton = ({ label, background, color }) => (
+  <StyledDangerButton background={background} color={color}>
+    {label}{" "}
+  </StyledDangerButton>
 );
 
-export const OutlinedButton = () => (
-  <StyledOutlinedButton> Outlined </StyledOutlinedButton>
+export const OutlinedButton = ({ label, background, color }) => (
+  <StyledOutlinedButton background={background} color={color}>
+    {label}
+  </StyledOutlinedButton>
 );
+
+PrimaryButton.propTypes = {
+  label: PropTypes.string.isRequired,
+  background: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+};
+
+SecondaryButton.propTypes = {
+  label: PropTypes.string.isRequired,
+  background: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+};
+
+DangerButton.propTypes = {
+  label: PropTypes.string.isRequired,
+  background: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+};
+
+OutlinedButton.propTypes = {
+  label: PropTypes.string.isRequired,
+  background: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+};
--- src/components/Button/stories/Button.stories.js
+++ src/components/Button/stories/Button.stories.js
@@ -15,20 +15,64 @@ export default {
   argTypes: {
     backgroundColor: { control: "color" },
     label: { control: "text" },
-  },
-  args: {
-    label: "Primary",
+    color: { control: "color" },
   },
 };
 
-export const PrimaryButtonStory = () => <PrimaryButton />;
+// eslint-disable-next-line react/jsx-props-no-spreading
+export const PrimaryButtonStory = (args) => (
+  <PrimaryButton
+    label={args.label}
+    background={args.backgroundColor}
+    color={args.color}
+  />
+);
 PrimaryButtonStory.storyName = "Primary";
+PrimaryButtonStory.args = {
+  label: "Primary",
+  backgroundColor: "#00a3ff",
+  color: "#ffffff",
+};
 
-export const SecondaryButtonStory = () => <SecondaryButton />;
+export const SecondaryButtonStory = (args) => (
+  <SecondaryButton
+    label={args.label}
+    background={args.backgroundColor}
+    color={args.color}
+  />
+);
 SecondaryButtonStory.storyName = "Secondary";
+SecondaryButtonStory.args = {
+  label: "Secondary",
+  backgroundColor: "#28a745",
+  color: "#ffffff",
+};
 
-export const DangerButtonStory = () => <DangerButton />;
+export const DangerButtonStory = (args) => (
+  <DangerButton
+    label={args.label}
+    background={args.backgroundColor}
+    color={args.color}
+  />
+);
 DangerButtonStory.storyName = "Danger";
+DangerButtonStory.args = {
+  label: "Danger",
+  backgroundColor: "#dc3545",
+  color: "#ffffff",
+};
 
-export const OutlineButtonStory = () => <OutlinedButton />;
+// eslint-disable-next-line react/jsx-props-no-spreading
+export const OutlineButtonStory = (args) => (
+  <OutlinedButton
+    label={args.label}
+    background={args.backgroundColor}
+    color={args.color}
+  />
+);
 OutlineButtonStory.storyName = "Outline";
+OutlineButtonStory.args = {
+  label: "Outline",
+  backgroundColor: "#ffffff",
+  color: "#007bff",
+};
