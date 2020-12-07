bbcd7dffb379470366b5ff6bd7cc4f3a366b7d93
Tayfun Aydogan <40833117+tayfuntayfun@users.noreply.github.com>
2020-12-07 22:07:08 +0000

    Button Variant added

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,115 +1,93 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Button = styled.button`
-  font-style: normal;
-`;
+  ${({ variant }) => css`
+    color: ${(props) => (props.color ? props.color : `#ffffff`)};
+    background-color: ${(props) =>
+      props.background ? props.background : `#00a3ff`};
+    border-color: #00a3ff;
+    width: 135.88px;
+    left: 2354px;
+    top: 1432px;
+    height: 42px;n
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    border: 1px solid transparent;
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    border-radius: 0.25rem;
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+      border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 
-export const StyledPrimaryButton = styled.button`
-  width: 135.88px;
-  left: 2354px;
-  top: 1432px;
-  height: 42px;
-  color: ${(props) => (props.color ? props.color : `#ffffff`)};
-  background-color: ${(props) =>
-    props.background ? props.background : `#00a3ff`};
-  border-color: #00a3ff;
-  font-weight: 400;
-  text-align: center;
-  white-space: nowrap;
-  vertical-align: middle;
-  border: 1px solid transparent;
-  padding: 0.375rem 0.75rem;
-  font-size: 1rem;
-  line-height: 1.5;
-  border-radius: 0.25rem;
-  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
-    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-`;
+    ${
+      variant === "secondary" &&
+      css`
+        color: ${(props) => (props.color ? props.color : `#ffffff`)};
+        background-color: ${(props) =>
+          props.background ? props.background : `#28a745`};
+        border-color: #28a745;
+      `
+    }
 
-export const StyledSecondaryButton = styled.button`
-  width: 135.88px;
-  left: 2354px;
-  top: 1432px;
-  height: 42px;
-  color: ${(props) => (props.color ? props.color : `#ffffff`)};
-  background-color: ${(props) =>
-    props.background ? props.background : `#28a745`};
-  border-color: #28a745;
-  font-weight: 400;
-  text-align: center;
-  white-space: nowrap;
-  vertical-align: middle;
-  border: 1px solid transparent;
-  padding: 0.375rem 0.75rem;
-  font-size: 1rem;
-  line-height: 1.5;
-  border-radius: 0.25rem;
-  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
-    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-`;
+    ${
+      variant === "danger" &&
+      css`
+        color: ${(props) => (props.color ? props.color : `#ffffff`)};
+        background-color: ${(props) =>
+          props.background ? props.background : `#dc3545`};
+        border-color: #28a745;
+      `
+    }
 
-export const StyledDangerButton = styled.button`
-  width: 135.88px;
-  left: 2354px;
-  top: 1432px;
-  height: 42px;
-  color: ${(props) => (props.color ? props.color : `#ffffff`)};
-  background-color: ${(props) =>
-    props.background ? props.background : `#dc3545`};
-  font-weight: 400;
-  text-align: center;
-  white-space: nowrap;
-  vertical-align: middle;
-  border: 1px solid transparent;
-  padding: .375rem .75rem;
-  font-size: 1rem;
-  line-height: 1.5;
-  border-radius: .25rem;
-  transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
-}`;
-
-export const StyledOutlinedButton = styled.button`
-  width: 135.88px;
-  left: 2354px;
-  top: 1432px;
-  height: 42px;
-  color: ${(props) => (props.color ? props.color : `#007bff`)};
-  background-color: ${(props) =>
-    props.background ? props.background : `#ffffff`};
-  font-weight: 400;
-  text-align: center;
-  white-space: nowrap;
-  vertical-align: middle;
-  border: 1px solid ${(props) => (props.color ? props.color : `#007bff`)};
-  padding: 0.375rem 0.75rem;
-  font-size: 1rem;
-  line-height: 1.5;
-  border-radius: 0.25rem;
-  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
-    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+      // Here the style that differ in the secondary
+    ${
+      variant === "outline" &&
+      css`
+        color: ${(props) => (props.color ? props.color : `#007bff`)};
+        background-color: ${(props) =>
+          props.background ? props.background : `#ffffff`};
+        border-color: ${(props) => (props.color ? props.color : `#007bff`)};
+      `
+    }
+  `}
 `;
 
 export const PrimaryButton = ({ ...args }) => (
-  <StyledPrimaryButton background={args.backgroundColor} color={args.color}>
+  <Button
+    variant="primary"
+    background={args.backgroundColor}
+    color={args.color}
+  >
     {args.label}
-  </StyledPrimaryButton>
+  </Button>
 );
 
 export const SecondaryButton = ({ ...args }) => (
-  <StyledSecondaryButton background={args.backgroundColor} color={args.color}>
+  <Button
+    variant="secondary"
+    background={args.backgroundColor}
+    color={args.color}
+  >
     {args.label}
-  </StyledSecondaryButton>
+  </Button>
 );
 
 export const DangerButton = ({ ...args }) => (
-  <StyledDangerButton background={args.backgroundColor} color={args.color}>
+  <Button variant="danger" background={args.backgroundColor} color={args.color}>
     {args.label}
-  </StyledDangerButton>
+  </Button>
 );
 
 export const OutlinedButton = ({ ...args }) => (
-  <StyledOutlinedButton background={args.backgroundColor} color={args.color}>
+  <Button
+    variant="outline"
+    background={args.backgroundColor}
+    color={args.color}
+  >
     {args.label}
-  </StyledOutlinedButton>
+  </Button>
 );
