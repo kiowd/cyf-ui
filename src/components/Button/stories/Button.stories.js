f823f219b10e4397d6a77ab20c4b4c77ad5558b5
Tayfun Aydogan <40833117+tayfuntayfun@users.noreply.github.com>
2020-12-08 22:28:07 +0000

    REmove Argument types and refactor

--- src/components/Button/stories/Button.stories.js
+++ src/components/Button/stories/Button.stories.js
@@ -1,54 +1,19 @@
 import React from "react";
-import {
-  Button,
-  PrimaryButton,
-  SecondaryButton,
-  DangerButton,
-  OutlinedButton,
-} from "..";
+import { Button } from "..";
 
 export default {
   title: "Components/Button",
   component: Button,
-  argTypes: {
-    backgroundColor: { control: "color" },
-    label: { control: "text" },
-    color: { control: "color" },
-  },
 };
 
-// eslint-disable-next-line react/jsx-props-no-spreading
-export const PrimaryButtonStory = (args) => <PrimaryButton {...args} />;
-PrimaryButtonStory.storyName = "Primary";
-PrimaryButtonStory.args = {
-  label: "Primary",
-  backgroundColor: "#00a3ff",
-  color: "#ffffff",
-};
+export const Primary = () => <Button variant="primary">Primary</Button>;
+Primary.storyName = "Primary";
 
-// eslint-disable-next-line react/jsx-props-no-spreading
-export const SecondaryButtonStory = (args) => <SecondaryButton {...args} />;
-SecondaryButtonStory.storyName = "Secondary";
-SecondaryButtonStory.args = {
-  label: "Secondary",
-  backgroundColor: "#28a745",
-  color: "#ffffff",
-};
+export const Secondary = () => <Button variant="secondary">Secondary</Button>;
+Secondary.storyName = "Secondary";
 
-// eslint-disable-next-line react/jsx-props-no-spreading
-export const DangerButtonStory = (args) => <DangerButton {...args} />;
-DangerButtonStory.storyName = "Danger";
-DangerButtonStory.args = {
-  label: "Danger",
-  backgroundColor: "#dc3545",
-  color: "#ffffff",
-};
+export const Danger = () => <Button variant="danger">Danger</Button>;
+Danger.storyName = "Danger";
 
-// eslint-disable-next-line react/jsx-props-no-spreading
-export const OutlineButtonStory = (args) => <OutlinedButton {...args} />;
-OutlineButtonStory.storyName = "Outline";
-OutlineButtonStory.args = {
-  label: "Outline",
-  backgroundColor: "#ffffff",
-  color: "#007bff",
-};
+export const Outline = () => <Button variant="outline">Outline</Button>;
+Outline.storyName = "Outline";
