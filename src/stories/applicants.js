e2e806accca163922321cf7b298c9cf312d48127
Davide Crestini <davide.crestini@theculturetrip.com>
2020-12-12 16:42:48 +0000

    feat: some fixes on table

---
+++ src/stories/applicants.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 1,
+    check: "true",
+    call: "true",
+    emoji: "happy",
+    name: "Serkan Cavus",
+    progress: "tick",
+    date: "date",
+  },
+  {
+    id: 2,
+    check: "true",
+    call: "true",
+    emoji: "happy",
+    name: "Someone else",
+    progress: "tick",
+    date: "date",
+  },
+];
